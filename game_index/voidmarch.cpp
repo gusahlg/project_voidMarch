8285a8d1399ed4ebcc4d35a5413159ab4480ce6a
gusahlg <gusahlg@gmail.com>
2025-05-11 14:27:31 +0100

    W commit

--- game_index/voidmarch.cpp
+++ game_index/voidmarch.cpp
@@ -1,12 +1,15 @@
 #include "raylib.h"
+Color gRed = {255, 20, 10, 255};
+Color spaceBlue = {20, 50, 240, 255};
 
-int main() {
-    InitWindow(800, 450, "Gustav’s raylib test");
-    while (!WindowShouldClose()) {
+int main(){
+    InitWindow(1500, 900, "Coolhetsstället");
+    while(!WindowShouldClose()){
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Hej världen!  FPS:", 20, 20, 20, DARKGRAY);
-        DrawFPS(160, 20);
+        ClearBackground(spaceBlue);
+        DrawText("Gutentag!", 20, 20, 100, gRed);
+        DrawText("FPS:", 1240, 16, 25, gRed);
+        DrawFPS(1300, 20);
         EndDrawing();
     }
     CloseWindow();
