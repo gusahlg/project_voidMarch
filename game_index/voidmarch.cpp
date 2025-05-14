4220dd5503a4d900cf8d513f38c5c6dbbdc87a8c
gusahlg <gusahlg@gmail.com>
2025-05-14 08:06:13 +0100

    Home screen is functioning, refining needed.

--- game_index/voidmarch.cpp
+++ game_index/voidmarch.cpp
@@ -2,9 +2,23 @@
 #include <iostream>
 Color gRed = {255, 20, 10, 255};
 Color spaceBlue = {25, 70, 240, 255};
+Color selectC = {5, 2, 33, 255};
+Color btnP = {3, 8, 54, 255};
+Color btnH = {14, 6, 92, 255};
+Color btnC = {27, 6, 107, 255};
+Rectangle button = {540, 300, 200, 75};
 void updateHome();
 void updateSelect();
 void updateGame();
+struct ButtonStyle{
+    Color bg[4];
+    Color fg[4];
+    Font  font;
+    float radius = 8; 
+    float padding = 12;
+    Texture2D image = {0};
+    Rectangle srcRect = {0}; 
+};
 class screen{
     public:
         int screenIdentifier;
@@ -21,28 +35,45 @@ class screen{
         }
     }
 };
+bool Hclicked = false;
 int main(){
     InitWindow(1280, 720, "PROJECT: VOIDMARCH");
+    SetTargetFPS(100);
     screen home; home.screenIdentifier = 1;
     screen select; select.screenIdentifier = 2;
     screen game; game.screenIdentifier = 3;
     while(!WindowShouldClose()){
-        home.update();
+        if(!Hclicked){
+            home.update();
+        }
+        else if(Hclicked){
+            select.update();
+        }
     }
     CloseWindow();
     return 0;
 }
 void updateHome(){
+    Vector2 mousePos = GetMousePosition();
+    bool hover  = CheckCollisionPointRec(mousePos, button);
+    bool pressed = hover && IsMouseButtonDown(MOUSE_LEFT_BUTTON);
+    bool clicked = hover && IsMouseButtonReleased(MOUSE_LEFT_BUTTON);
+    if(clicked){
+        Hclicked = true;
+    }
+    Color Cstate = pressed ? btnP : hover ? btnH : btnC;
     BeginDrawing();
     ClearBackground(spaceBlue);
     DrawText("PROJECT VOIDMARCH", 20, 20, 75, BLACK);
     DrawText("FPS:", 1100, 20, 20, gRed);
     DrawFPS(1150, 20);
+    DrawRectangleRec(button, Cstate);
+    DrawText("SETUP GAME", 545, 320, 28, gRed);
     EndDrawing();
 }
 void updateSelect(){
     BeginDrawing();
-    ClearBackground(GREEN);
+    ClearBackground(selectC);
     DrawText("PROJECT VOIDMARCH", 20, 20, 75, BLACK);
     DrawText("FPS:", 1100, 20, 20, gRed);
     DrawFPS(1150, 20);
