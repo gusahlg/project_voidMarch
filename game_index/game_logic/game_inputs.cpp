8ca3d4b23a5a0a6f46bfb4fd6fbc479b7ad27033
gusahlg <gusahlg@gmail.com>
2025-06-09 16:14:04 +0100

    Adding in so that the projectile positions are saved so that there can be many projectiles at once.

--- game_index/game_logic/game_inputs.cpp
+++ game_index/game_logic/game_inputs.cpp
@@ -75,14 +75,33 @@ float projX = 0;
 float projY = 0;
 float dirX, dirY;
 bool projActive = false;
+bool targetHit = false;
 float projectileSpeed = 300.0f;
-float projW = 50;
-float projH = 50;
+float projW = 20;
+float projH = 15;
+
+int projCount = 1;
 void spawnProjectile(float x, float y, float w, float h, float dt, Level& lvl){
-    DrawRectangle(x, y, projW, projH, RED);
+    if(collisionRect(x, y, projW, projH, lvl)){
+        targetHit = true;
+    }
+    // Gotta add in so that the positions of all projectiles are remembered and referenced correctly.
+    else{
+        DrawRectangle(x, y, projW, projH, RED);
+    }
 }
 void updateRangedAttack(float x, float y, float dt, Level& lvl){
-    /* Gonna add in stuff for drawing in the actual weapon as well.
+    /* Gonna add in stuff for drawing in the actual weapon as well. 
     DrawTexturePro()*/
-    spawnProjectile(x, y, projW, projX, dt, lvl);
+    for(int i = 0; i < projCount; ++i){
+        spawnProjectile(x, y, projW, projX, dt, lvl);
+    }
+    if(!targetHit){
+        projCount += 1;
+    }
+    else{
+        if(projCount < 1){
+            projCount -= 1;
+        }
+    }
 }
