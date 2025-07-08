37ba15b6f1e66946a2bb50db007f4822678c8672
gusahlg <gusahlg@gmail.com>
2025-07-08 18:55:26 +0100

    Working on data saving and player stats.

---
+++ game_index/game_logic/on_screen.cpp
@@ -0,0 +1,1 @@
+// This is where UI and on screen stuff is handled.
--- game_index/game_logic/player_event_handler.cpp
+++ game_index/game_logic/player_event_handler.cpp
@@ -10,18 +10,3 @@
 #include <cmath>
 #include <raymath.h>
 #include <cstdint>
-// This is for handling different player states, stats (two similar words after each other, don't fall asleep my fellow 'stay up late gamedev') such as health and attributes such as 'intelligence'.
-enum class Stat : uint8_t{
-    HP, // Hitpoints 
-    MP, // Mana or something
-    STR,// Strength
-    AGI,// Agility
-    INT,// Intelligence
-    _COUNT
-};
-struct Stats{
-    static constexpr size_t COUNT = static_cast<size_t>(Stat::_COUNT);
-    
-
-};
-
