ce01580290d2ceef76e24f33b0d368a5b8cc6893
gusahlg <gusahlg@gmail.com>
2025-08-06 16:44:58 +0200

    Added in a rust playground folder and fixed some scaling issues.

---
+++ rust/target/debug/deps/rust-9cb474c3d0fe6748.d
@@ -0,0 +1,5 @@
+C:\Users\gusta\Desktop\project_voidMarch\rust\target\debug\deps\rust-9cb474c3d0fe6748.d: src\main.rs
+
+C:\Users\gusta\Desktop\project_voidMarch\rust\target\debug\deps\librust-9cb474c3d0fe6748.rmeta: src\main.rs
+
+src\main.rs:
